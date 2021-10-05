5bb6e4fbad0f52b06fe4d8c7b16612e305b46d90
JunKangWong <88149192+JunKangWong@users.noreply.github.com>
2021-10-05 17:02:11 +0800

    Correction made

--- Assignment02/task2/task2-29801036.c
+++ Assignment02/task2/task2-29801036.c
@@ -12,18 +12,15 @@ int main(){
 	shortest_process_remaining_next(&event_occur, &event_record);
 	
 	// generate an output file according to the recorded events in event_record queue.
-	generate_output_file("results-task2.txt", &event_record);
+	generate_output_file("results-2.txt", &event_record);
 }
 
 
 
 void shortest_process_remaining_next(Queue *event_occur, Queue *event_record){
-	int current_time = 0;
+	int time_quant = 0, current_time = 0, active_process = 0;
 	bool completed = true;
-	int active_process = 0;
 	pcb_t running_process;
-	pcb_t arrive;
-	int time_quant;
 	
 	// initialise a priority queue (min heap) based on remainingTime.
 	Heap* ready = init_heap(SIZE);
@@ -34,61 +31,85 @@ void shortest_process_remaining_next(Queue *event_occur, Queue *event_record){
 		
 		// simulates arrival of process
 		while(event_occur->value[(event_occur->front)+1].entryTime == current_time){
-			arrive = dequeue(event_occur);
-			arrive.current_time = current_time; // record time of event occurence.
-			insert_heap(ready, arrive);			// insert newly arrived process into ready queue.
+			to_ready_queue(ready, event_occur);
 			active_process ++;
-			
-			enqueue(event_record, arrive); 				// record occurence of arrive event into event log queue.
 		}
 		
 		// simulate running of processes
 		if (!completed){
 			running_process.remainingTime --;
 			time_quant --;
-		
+			
 			if(running_process.remainingTime == 0){
+				completed = terminate_process(event_record, &running_process, current_time);
 				active_process --;
-				running_process.state = TERMINATED;
-				completed = true;
+
 			}else if(time_quant == 0){
 				// if running process is still the highest priority process after time quantum is over.
-				// continue to run.
+				// continue to run --> reset time quantum
 				if(is_higher_priority(running_process, get_root(ready))){
-					time_quant = TIME_QUANTUM; // reset time quantum
-					completed = false;
+					time_quant = TIME_QUANTUM; 
 				}else{
-					// insert running process back to priority queue if its not completely terminated.
-					insert_heap(ready, running_process);
-					running_process.state = EXIT;
-					completed = true;
+					completed = preempt_process(ready, &running_process, current_time); 
 				}
 			}
-			// record event if process completed or process preempted.
-			if(completed){
-				running_process.current_time = current_time;
-				enqueue(event_record, running_process);
-			}
 		}
-
 		// simulates scheduling of process
 		// if current process completes running andready queue has element to be scheduled.
 		if(ready->size > 0 && completed){
 			time_quant = TIME_QUANTUM; // reset time quantum
-			running_process = extract_root(ready); 
-			running_process.state = RUNNING;
-			running_process.current_time = current_time;
-			completed = false;
-			
-			// record occurence of event
-			enqueue(event_record, running_process);
+			completed = schedule_process(ready, &running_process, current_time);
 		}
-		
 		// clock tick by 1 second
 		current_time ++;
 	}
-	
 	// free up memory of ready queue.
 	free_heap(ready);
 }
 
+
+
+bool schedule_process(Heap *ready, pcb_t *running_process, int current_time){
+	// update firstServedTime only when remainingTime equals to firstServedTime.
+	*running_process = extract_root(ready);
+	running_process->firstServedTime = (running_process->remainingTime == running_process->serviceTime) ?
+	 current_time : running_process->firstServedTime;
+	running_process->state = RUNNING;
+	running_process->event_time = current_time;
+	
+	// record occurence of event
+	print_event(*running_process);
+	return false;
+}
+
+
+
+
+bool terminate_process(Queue *event_record, pcb_t *running_process, int current_time){
+	running_process->state = TERMINATED;
+	running_process->terminateTime = current_time;
+	enqueue(event_record, *running_process);
+	
+	print_event(*running_process);
+	return true;
+}
+
+
+
+
+bool preempt_process(Heap *ready, pcb_t *running_process, int current_time){
+	// insert running process back to priority queue if its not completely terminated.
+	insert_heap(ready, *running_process);
+	running_process->state = READY;
+	running_process->event_time = current_time;
+	
+	return true;
+}
+
+
+
+void to_ready_queue(Heap *ready, Queue *event_occur){
+	pcb_t arrive = dequeue(event_occur);
+	insert_heap(ready, arrive);			// insert newly arrived process into ready queue.
+	print_event(arrive);
+}
