65db2e8b3bc69a6557c358381a35043df6ba3ca6
JunKangWong <88149192+JunKangWong@users.noreply.github.com>
2021-10-07 21:36:16 +0800

    Add files via upload

--- Assignment02/Submission/change_process_state.h
+++ Assignment02/Submission/change_process_state.h
@@ -1,3 +1,12 @@
+/*
+Name			: Wong Jun Kang
+StudentID		: 29801036
+Date created	: 01/10/2021
+Last Modified	: 07/10/2021
+
+The header file of change_process_state.c
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
